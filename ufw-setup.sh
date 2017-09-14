3f639dc8018082a9955e3b61c53c1226d50104dd
BadgerPete <pete@uwalumni.com>
2017-09-14 13:39:53 -0500

    Create ufw-setup.sh
    Initial install, allow ssh on port 22 form local hosts

---
+++ ufw-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+ips-v4="192.168.1.0/24"
+ips-v6=""
+
+apt install ufw -y > /dev/null
+ufw enable > /dev/null
+
+for ip in ips-v4; do ufw allow from $ip to any port 22 proto tcp; done
+#for ip in ips-v6; do ufw allow from $ip; done
+
+ufw reload > /dev/null
+
+# Examples to retrict to port 80
+#for ip ip ips-v4; do ufw allow from $ip to any port 80 proto tcp; done
+#for ip in ips-v6; do ufw allow from $ip to any port 80 proto tcp; done
