5aa00916504cbcd0bd4028057c6e1f54660bad0c
BadgerPete <pete@uwalumni.com>
2017-09-14 13:59:01 -0500

    Update ufw-plex.sh

--- ufw-plex.sh
+++ ufw-plex.sh
@@ -1,19 +1,18 @@
-#!/bin/sh
-
-ips-v4="192.168.1.0/24"
-ips-v6=""
+#!/bin/bash
 
 apt install ufw -y > /dev/null
 ufw enable > /dev/null
 
-for ip in ips-v4; do ufw allow from $ip to any port 22 proto tcp; done
-for ip in ips-v4; do ufw allow from $ip to any port 40365 proto tcp; done
-for ip in ips-v4; do ufw allow any to any port 32400 proto tcp; done
+ipsv4="192.168.1.0/24"
+ipsv6=""
 
-#for ip in ips-v6; do ufw allow from $ip; done
+for ip in $ipsv4
+do
+  echo -n "$ip "
+  ufw allow from $ip to any port 80 proto tcp
+  ufw allow from $ip to any port 40365 proto tcp
+done
+
+ufw allow any to any port 32400 proto tcp
 
 ufw reload > /dev/null
-
-# Examples to retrict to port 80
-#for ip ip ips-v4; do ufw allow from $ip to any port 80 proto tcp; done
-#for ip in ips-v6; do ufw allow from $ip to any port 80 proto tcp; done
